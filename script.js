7d8b40611426ce65fd465d76babe8e8382da786e
IbrahimMohram <45979452+IbrahimMohram@users.noreply.github.com>
2021-05-12 04:55:48 +0200

    firstCommit

---
+++ script.js
@@ -0,0 +1,37 @@
+var counted = 0;
+$(window).scroll(function() {
+
+  var oTop = $('.fun-facts-area').offset().top - window.innerHeight;
+  if (counted == 0 && $(window).scrollTop() > oTop) {
+    $('.counter').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-target');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 5000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    counted = 1;
+  }
+
+});
+
+window.addEventListener('scroll',function(){
+	let header = document.querySelector('header');
+	header.classList.toggle('scrolling-active' , window.scrollY >0);
+	})
